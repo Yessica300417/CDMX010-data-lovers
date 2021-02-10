1bb1993eb8cb8e0b389136bef6f7298b6c4d5577
andreaGuzes <skarcega@hotmail.com>
2021-02-10 09:25:45 -0600

    ultimos cambios

--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 //Este Archivo llamado data.js tiene el proposito de manipular la DATA
 export function filterGender(characters,genero){
-  const personajes= characters.filter(role => role.gender===genero);
-  return personajes
-}
+  const genderRole= characters.filter(role => role.gender===genero);
+  return genderRole
+};
 
 export function filterStatus(characters,status){
   const statusRole = characters.filter(role => role.status===status);
   return statusRole
-}
+};
 // export function orderAZ(characters){
 //   const personajes2= characters.sort((a,b) => {
 //     if (a.name < b.name){
@@ -20,8 +20,9 @@ export function filterStatus(characters,status){
 //     });
 //     return personajes2
 // };
+
 export function orderZA(characters){
-  const personajes4= characters.sort((c,d) => {
+  const sortRole= characters.sort((c,d) => {
     if (c.name < d.name){
 return 1;
     }
@@ -30,6 +31,6 @@ return 1;
     }
 return 0;
     });
-    return personajes4
-  }
+    return sortRole
+  };
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 //MAIN.JS Este bendito archivo tiene el proposito de manipular el DOM
 import data from './data/rickandmorty/rickandmorty.js';
-import {filterGender, filterStatus, orderZA /*orderAZ*/} from './data.js'//invocacion
+import {filterGender, filterStatus, orderZA} from './data.js'//invocacion
 
 //VISUALIZACION DE LAS TARJETAS
-const characters = data.results;  
-
+const characters = data.results;
 
      function Card(personaje) {
           return `
@@ -23,117 +22,46 @@ const characters = data.results;
           `
      }
 
-    // FILTRADO 
-     function mostrarTarjetas(personajes) { // female, male\
-          const cards =  document.getElementById('cardsContainer')
-
-          let html = ''
+    //Mostrar  tarjetas 
+     function cardShow(personajes) {
+          const cards =  document.getElementById('cardsContainer');
+          let information = ''; 
           personajes.forEach((personaje) => {
-               html += Card(personaje)
-          });
+              information += Card(personaje);
+            });
+            cards.innerHTML = information;
+        };
 
-          cards.innerHTML = html;
-     }
-// function Card(characters) {
-//     return characters
-// }
-
-let buttonPe = document.getElementById("buttonPe");
-     buttonPe.addEventListener("click", () => {
-        const personnajes = mostrarTarjetas (characters);
-        mostrarTarjetas(personajes);
-     });
-  
-
-     let button = document.getElementById("female");
-     button.addEventListener("click", () => {
-          const personajes = filterGender(characters, 'Female'); 
-
-          mostrarTarjetas(personajes)
-     });
+        cardShow(characters);
+        
+        let button = document.getElementById("female");
+        button.addEventListener("click", () => {
+            const charactersFem = filterGender(characters, 'Female'); 
+            cardShow(charactersFem)
+        });
 // FILTRADO POR MASCULINO
     
     let maleButton = document.getElementById("male");
-
     maleButton.addEventListener("click", () => {
-          const personajes = filterGender(characters, 'Male'); 
-
-        mostrarTarjetas(personajes)
+        const charactersMa = filterGender(characters, 'Male'); 
+        cardShow(charactersMa);
     });
 
     let aliveButton = document.getElementById("alive");
-
     aliveButton.addEventListener("click", () => {
-          const personajes = filterStatus(characters, 'Alive'); 
-
-        mostrarTarjetas(personajes)
+        const charactersAli = filterStatus(characters, 'Alive');
+        cardShow(charactersAli);
     });
 
     let deadButton = document.getElementById("dead");
-
     deadButton.addEventListener("click", () => {
-          const personajes = filterStatus(characters, 'Dead'); 
-
-        mostrarTarjetas(personajes)
+        const charactersDe = filterStatus(characters, 'Dead');
+        cardShow(charactersDe);
     });
+
     //ORDENADO
     let buttonZa = document.getElementById("za");
-
     buttonZa.addEventListener("click", () => {
-          const personajes = orderZA(characters,); 
-
-        mostrarTarjetas(personajes)
+        const charactersOr = orderZA(characters);
+        cardShow(charactersOr);
     });
-
-
-   
-    
-// CALCULO DE LA APARICION DE LOS PERSONAJES POR CAPITULO 
-// function reglaDeTres (capitulos) {
-//      let resultado = (capitulos*100)/31
-//     console.log(resultado)
-// };
-// reglaDeTres (data.results[0].episode.length);
-// reglaDeTres(data.results[1].episode.length);
-// reglaDeTres(data.results[2].episode.length);
-// reglaDeTres(data.results[46].episode.length);
-// reglaDeTres(data.results[117].episode.length);
-// reglaDeTres(data.results[239].episode.length);
-// reglaDeTres(data.results[241].episode.length);
-// reglaDeTres(data.results[330].episode.length);
-
-//Grafica de resultados
-
-// google.charts.load("current", {packages:["corechart"]});
-// google.charts.setOnLoadCallback(drawChart);
-// function drawChart() {
-//   var data = google.visualization.arrayToDataTable([
-//     ["Personaje", "Porcentaje", { role: "style" } ],
-//     ["Rick Sanchez", 100, "#b87333"],
-//     ["Morty Smith", 100, "color: #C70039"],
-//     ["Summer Smith", 77.41, "color:#76A7FA"],
-//     ["Birdperson", 16.12, "color: #e5e4e2"]
-    //["Evil Morty", 6.45, "color: #e5e4e2"]
-    //["Mr. Goldenflod", 29.03, "color: #b87333"],
-//     ["Mr. Meeseeks", 12.90, "color:BC5679"],
-//     ["Squanchy", 9.67, "color: #e5e4e2"]
-//  ]);
-
-//  var view = new google.visualization.DataView(data);
-//   view.setColumns([0, 1,
-//                    { calc: "stringify",
-//                      sourceColumn: 1,
-//                      type: "string",
-//                      role: "annotation" },
-//                    2]);
-
-//   var options = {
-//     title: "Porcentaje de aparición en la serie",
-//     width: 600,
-//     height: 400,
-//     bar: {groupWidth: "95%"},
-//     legend: { position: "none" },
-//   };
-//   var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
-//   chart.draw(view, options);
-// }
